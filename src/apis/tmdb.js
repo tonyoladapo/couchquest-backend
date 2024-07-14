8b1c420e0b652b4be3f3693d98ddfbf9fccf7d52
Tony <toneyobaze@gmail.com>
2024-07-14 14:28:39 +0200

    chore: Update TMDB and Trakt API base URLs
    This commit updates the TMDB and Trakt API base URLs in the `tmdb.js` and `trakt.js` files respectively. The base URLs are now retrieved from environment variables (`process.env.TMDB_API_BASE_URL` and `process.env.TRAKT_API_BASE_URL`) instead of being hard-coded. These changes were made to improve flexibility and allow for easier configuration of the API endpoints.

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const tmdb = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
+  baseURL: process.env.TMDB_API_BASE_URL,
   params: {
     api_key: process.env.TMDB_API_KEY,
   },
